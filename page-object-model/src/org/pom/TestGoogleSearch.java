24a27809a02bb8f352b6470d7afe0e9872516fdd
Gaurav Singh <gaurav.singh55@gmail.com>
2018-02-04 07:08:52 +0530

    Simple test ng demonstration

---
+++ page-object-model/src/org/pom/TestGoogleSearch.java
@@ -0,0 +1,16 @@
+package org.pom;
+
+import org.testng.annotations.Test;
+
+public class TestGoogleSearch {
+
+    @Test
+    public void testGoogleSearch() {
+        System.out.println("Basic Search");
+    }
+
+    @Test
+    public void testGoogleSearchAnother() {
+        System.out.println("Advanced Search");
+    }
+}
